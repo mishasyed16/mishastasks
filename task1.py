2794fc009fa023364a4a928c9ec3d0e20fe9468a
mishasyed16 <74056170+mishasyed16@users.noreply.github.com>
2020-11-06 18:36:32 +0530

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,5 @@
+
+t = 1.1
+r = 3.14 * t * t
+print('Input the radius:', +t)
+print('The area of circle with radius {0} is: {1}'.format(t,r))
---
+++ task2.py
@@ -0,0 +1,5 @@
+in1 = "abc.py"
+in2 = "python"
+print("Input the filename:" in1)
+print("The extension of the file:" in2)
+
